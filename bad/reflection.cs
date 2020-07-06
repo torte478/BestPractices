fbb9285e278a6a8d23d452eef0109d515adc08ae
torte478 <torte478@gmail.com>
2020-07-06 08:55:23 +0300

    feature (bad): add field reflection case

---
+++ bad/reflection.cs
@@ -0,0 +1,14 @@
+var fields = GetType()
+            .GetFields(BindingFlags.Instance | BindingFlags.Public);
+foeach (var field in fields)
+{
+    var value = field.GetValue(this);
+    var def = Activator.CreateInstance(value.GetType()); //It gets stuck
+    if (value == def) //It's not working (boxing)
+        throw new Exception($"Field {field.Name} is not defined!");
+}
+
+//=======================================
+var def = Activator.CreateInstance(field.FieldType); //Better, but not works for strings
+if (value.Equal(def)) //It's ok
+    throw new Exception($"Field {field.Name} is not defined!");
